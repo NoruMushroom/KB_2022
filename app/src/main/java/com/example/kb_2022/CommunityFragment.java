daca0a02a92ee8baf23ec811b3da17af9c521760
NoRu <kimminku98@naver.com>
2022-07-12 01:02:42 +0900

    커뮤니티창

--- app/src/main/java/com/example/kb_2022/CommunityFragment.java
+++ app/src/main/java/com/example/kb_2022/CommunityFragment.java
@@ -1,5 +1,8 @@
 package com.example.kb_2022;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.RequiresPermission;
@@ -15,6 +18,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CommunityFragment#newInstance} factory method to
@@ -24,9 +39,13 @@ public class CommunityFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private ListView Community_List;
+    private Context This_Activity;
+    private static String IP_ADDRESS = "http://123.215.162.92/KBServer/readboard.php";
+    private static String TAG = "phptest";
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private String mJsonString;
+    private ArrayList<Community_Type> mArrayList;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -65,6 +84,7 @@ public class CommunityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        This_Activity = container.getContext();
         View Community_View = inflater.inflate(R.layout.fragment_community, container, false);
         Community_List = Community_View.findViewById(R.id.Community_ListView);
         ImageButton Write_icon = Community_View.findViewById(R.id.Write);
@@ -72,16 +92,110 @@ public class CommunityFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Log.i("STATE", "LOG in SERVER");
-            }
+            }//글쓰기 서버 업로드
         });
         dataSetting();
         return Community_View;
     }
     private void dataSetting(){
+        GetData task = new GetData();
+        task.execute(IP_ADDRESS, "");
+    }
+    private class GetData extends AsyncTask<String, Void, String>{
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(This_Activity,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            if (result == null){
+                Log.d(TAG, "response - " + result);
+                //실패시
+            }
+            else {
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String serverURL = params[0];
+            String postParameters = params[1];
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString().trim();
+            } catch (Exception e) {
+                errorString = e.toString();
+                return null;
+            }
+        }
+    }
+    private void showResult() {
+        String TAG_JSON = "board_list";
+        String TAG_BNO = "bno";
+        String TAG_TITLE = "title";
+        String TAG_UNAME = "name";
+        String TAG_CONTENT = "content";
+        String TAG_LIKE = "islike";
         CommunityList_Adapter List_item = new CommunityList_Adapter();
-        for (int i=0; i<3; i++) {
-            List_item.addItem("작성자","홍길동",3,1);
-        }//여기서 DB접근
-        Community_List.setAdapter(List_item);
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject item = jsonArray.getJSONObject(i);
+                Integer bno = item.getInt(TAG_BNO);
+                String title = item.getString(TAG_TITLE);
+                String uname = item.getString(TAG_UNAME);
+                String content = item.getString(TAG_CONTENT);
+                Integer like = item.getInt(TAG_LIKE);
+                List_item.addItem(title,uname,like,bno);
+                System.out.println(bno + title + uname + content + like);
+            }
+            Community_List.setAdapter(List_item);
+        } catch (JSONException e) {
+            Log.d(TAG, "showResult : ", e);
+        }
     }
 }
