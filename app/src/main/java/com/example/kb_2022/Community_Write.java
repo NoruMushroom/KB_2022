a3627102336328ea66e8aef88b4e18e086c3a67d
leetaggg <leetag1163@naver.com>
2022-07-13 15:19:53 +0900

    Http 통신

--- app/src/main/java/com/example/kb_2022/Community_Write.java
+++ app/src/main/java/com/example/kb_2022/Community_Write.java
@@ -1,8 +1,13 @@
 package com.example.kb_2022;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,10 +46,106 @@ public class Community_Write extends AppCompatActivity {
         userName = intent.getStringExtra("이름");
         Title = findViewById(R.id.W_title);
         Title.setSelection(0);
-        Content = findViewById(R.id.W_content);
-        Content.setSelection(0);
         PW = findViewById(R.id.W_pw);
         PW.setSelection(0);
+        Content = findViewById(R.id.W_content);
+        Content.setSelection(0);
         Save = findViewById(R.id.W_save);
+
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetData task = new GetData();
+                task.execute("writeboard", Title.getText().toString(), userName, PW.getText().toString(), Content.getText().toString());
+            }
+        });
+    }
+    private class GetData extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(Community_Write.this,
+                    "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+
+
+            if (result == null){
+                Log.d(TAG, "response - " + result);
+            }
+            else {
+                mJsonString = result;
+                showResult();
+            }
+
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String serverURL = "http://123.215.162.92/KBServer/" + params[0] + ".php";
+            String title = params[1];
+            String uname = params[2];
+            String uPassword = params[3];
+            String content = params[4];
+
+            String postParameters = "title=" + title + "&uname=" + uname + "&upw=" + uPassword + "&content=" + content;
+
+
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "응답코드 : " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString().trim();
+            } catch (Exception e) {
+                errorString = e.toString();
+                return null;
+            }
+        }
+    }
+    private void showResult() {
+        String TAG_SUCCESS = "success";
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+
+            String success = jsonObject.getString(TAG_SUCCESS);
+            System.out.println(success);
+
+        } catch (JSONException e) {
+            Log.d(TAG, "showResult : ", e);
+        }
     }
 }
