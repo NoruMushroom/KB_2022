2f5779256d840c9fd6d4174e10d89e18dabb93c5
NoRu <kimminku98@naver.com>
2022-07-14 14:39:14 +0900

    커뮤니티창

--- app/src/main/java/com/example/kb_2022/CalendarFragment.java
+++ app/src/main/java/com/example/kb_2022/CalendarFragment.java
@@ -1,13 +1,33 @@
 package com.example.kb_2022;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
+import com.prolificinteractive.materialcalendarview.format.TitleFormatter;
+
+import org.threeten.bp.LocalDate;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +38,7 @@ public class CalendarFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private MaterialCalendarView calendarView;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -60,8 +81,60 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View Calender_View = inflater.inflate(R.layout.fragment_calendar, container, false);
-        TextView text = Calender_View.findViewById(R.id.Calender_text);
-        text.setText("여기는 캘린더 화면 입니다.");
+        calendarView = Calender_View.findViewById(R.id.Calendar);
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+        calendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader);
+        calendarView.setOnRangeSelectedListener(new OnRangeSelectedListener() {
+            @Override
+            public void onRangeSelected(@NonNull MaterialCalendarView widget, @NonNull List<CalendarDay> dates) {
+                // 아래 로그를 통해 시작일, 종료일이 어떻게 찍히는지 확인하고 본인이 필요한 방식에 따라 바꿔 사용한다
+                // UTC 시간을 구하려는 경우 이 라이브러리에서 제공하지 않으니 별도의 로직을 짜서 만들어내 써야 한다
+                String startDay = dates.get(0).getDate().toString();
+                String endDay = dates.get(dates.size() - 1).getDate().toString();
+                Log.e(TAG, "시작일 : " + startDay + ", 종료일 : " + endDay);
+            }
+        });
+        calendarView.addDecorators(new DayDecorator(container.getContext()));
+
+        // 좌우 화살표 가운데의 연/월이 보이는 방식 커스텀
+        calendarView.setTitleFormatter(new TitleFormatter() {
+            @Override
+            public CharSequence format(CalendarDay day) {
+                // CalendarDay라는 클래스는 LocalDate 클래스를 기반으로 만들어진 클래스다
+                // 때문에 MaterialCalendarView에서 연/월 보여주기를 커스텀하려면 CalendarDay 객체의 getDate()로 연/월을 구한 다음 LocalDate 객체에 넣어서
+                // LocalDate로 변환하는 처리가 필요하다
+                LocalDate inputText = day.getDate();
+                String[] calendarHeaderElements = inputText.toString().split("-");
+                StringBuilder calendarHeaderBuilder = new StringBuilder();
+                calendarHeaderBuilder.append(calendarHeaderElements[0])
+                        .append(" ")
+                        .append(calendarHeaderElements[1]);
+                return calendarHeaderBuilder.toString();
+            }
+        });
         return Calender_View;
     }
+    /* 선택된 요일의 background를 설정하는 Decorator 클래스 */
+        private static class DayDecorator implements DayViewDecorator {
+
+            private final Drawable drawable;
+
+            public DayDecorator(Context context) {
+                drawable = ContextCompat.getDrawable(context, R.drawable.calendar_selector);
+            }
+
+            // true를 리턴 시 모든 요일에 내가 설정한 드로어블이 적용된다
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                return true;
+            }
+
+            // 일자 선택 시 내가 정의한 드로어블이 적용되도록 한다
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.setSelectionDrawable(drawable);
+    //            view.addSpan(new StyleSpan(Typeface.BOLD));   // 달력 안의 모든 숫자들이 볼드 처리됨
+            }
+        }
 }
