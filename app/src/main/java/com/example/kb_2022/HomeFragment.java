f6fc383036b2d6fc6112113cf9945045ed4ea42b
leetaggg <leetag1163@naver.com>
2022-08-02 00:14:45 +0900

    Http 통신

--- app/src/main/java/com/example/kb_2022/HomeFragment.java
+++ app/src/main/java/com/example/kb_2022/HomeFragment.java
@@ -50,6 +50,7 @@ public class HomeFragment extends Fragment {
     private ArrayList[] Chart_List = {Daily_chart,Weekly_chart,Monthly_chart};
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private  JSONObject item;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -215,5 +216,7 @@ public class HomeFragment extends Fragment {
                 return null;
             }
         }
+
+
     }
 }
