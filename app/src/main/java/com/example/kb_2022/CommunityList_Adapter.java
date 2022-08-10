3f1c420ce1632485e04a18eb10af982f3afcf4cd
NoRu <kimminku98@naver.com>
2022-08-10 15:43:10 +0900

    그래프 완성

--- app/src/main/java/com/example/kb_2022/CommunityList_Adapter.java
+++ app/src/main/java/com/example/kb_2022/CommunityList_Adapter.java
@@ -49,9 +49,9 @@ public class CommunityList_Adapter extends BaseAdapter {
         TextView Writer = convertView.findViewById(R.id.Writer);
         Community_Type myItem = getItem(position);
         Title.setText(myItem.getTitle());
-        Like.setText("좋아요: " + myItem.getLike());
-        Number.setText("글번호:" + myItem.getNumber());
-        Writer.setText("작성자: " + myItem.getWriter());
+        Like.setText("좋아요 : " + myItem.getLike());
+        Number.setText("글번호 : " + myItem.getNumber());
+        Writer.setText("작성자 : " + myItem.getWriter());
         return convertView;
     }
 
